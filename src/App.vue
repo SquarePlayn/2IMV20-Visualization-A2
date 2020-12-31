989dbf53ea0a7fce6896f03e4fd846b65d234b12
Pieter Voors <pietervoors@hotmail.com>
2020-12-31 16:59:20 +0100

    Add Bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+
+    <b-container>
+      <b-card header="Some card">Here is a Bootstrap-vue card</b-card>
+    </b-container>
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
-}
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+// Import Vue and other plugins
+import Vue from 'vue';
+import {BootstrapVue, IconsPlugin} from "bootstrap-vue";
 
-Vue.config.productionTip = false
+// Import CSS
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Import components & store
+import App from './App.vue';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    store,
+    render: h => h(App)
+}).$mount('#app');
